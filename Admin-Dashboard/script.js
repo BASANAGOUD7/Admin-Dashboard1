d67ac6badda911c040efe2309d7a11eeb11cf497
Basanagoud Patil <basanagoudpatil43@gmail.com>
2026-01-14 23:11:03 +0530

    Admin Dashboard

--- Admin-Dashboard/script.js
+++ Admin-Dashboard/script.js
@@ -1,6 +1,7 @@
 // Sidebar Toggle
 const hamburger = document.querySelector(".toggle-btn");
 const toggler = document.querySelector("#icon");
+
 hamburger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
   toggler.classList.toggle("bxs-chevrons-right");
@@ -8,13 +9,12 @@ hamburger.addEventListener("click", function () {
 });
 
 // Toggle Sidebar Dropdown
-
 const items = document.querySelectorAll(".has-dropdown");
 
 function toggle() {
   const itemToggle = this.classList.contains("active");
 
-  for (i = 0; i < items.length; i++) {
+  for (let i = 0; i < items.length; i++) {
     items[i].classList.remove("active");
   }
 
@@ -25,3 +25,39 @@ function toggle() {
 
 items.forEach((item) => item.addEventListener("click", toggle));
 
+// User link dropdown
+const userProfile = document.querySelector(".user-link");
+
+userProfile.addEventListener("click", function () {
+  document
+    .querySelector(".user-link-dropdown")
+    .classList.toggle("show");
+});
+
+// Chart.js Bar Chart
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  },
+});
+
+//DataTableJS
+
+let table = new DataTable("#main-table", {
+  pageLength:5, 
+})
